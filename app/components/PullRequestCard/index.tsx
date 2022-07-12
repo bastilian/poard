24130b21bc7150436a4525845e096fbd7303604e
Sebastian Gräßl <mail@bastilian.me>
2022-07-12 23:47:26 +0200

    fix: Use momentjs for date calculations

--- app/components/PullRequestCard/index.tsx
+++ app/components/PullRequestCard/index.tsx
@@ -1,17 +1,16 @@
 
 import { Card, CardBody, CardFooter, CardTitle, Grid, GridItem, Label, Text } from '@patternfly/react-core/dist/umd/react-core';
-import { formatDistance } from 'date-fns';
+import moment from 'moment';
 import Avatar from 'react-avatar';
 import UpdateBy from './components/UpdatedBy';
 import CustomPullRequestTags from './components/CustomPullRequestTags';
 import { age, labelColor } from './helpers';
 
 const PullRequestCard = (pullRequest) => {
-  const { title, updatedAt, createdAt, author, number, body, url, repository, ...rest } = pullRequest;
-  // TODO Fix date calculations
-  const updatedAtDate = updatedAt && new Date(updatedAt);
-  const createdAtDate = createdAt && new Date(createdAt);
-  const modifiedAt = updatedAtDate || createdAtDate || new Date();
+  const { title, updatedAt, createdAt, author, number, url } = pullRequest;
+  const updatedAtDate = updatedAt && moment(updatedAt);
+  const createdAtDate = createdAt && moment(createdAt);
+  const modifiedAt = updatedAtDate || createdAtDate;
 
   return (
     <GridItem span={4}>
@@ -25,18 +24,11 @@ const PullRequestCard = (pullRequest) => {
               </a>
             </GridItem>
             <GridItem span={1} style={{ textAlign: 'right' }}>
-              <Label color={labelColor(modifiedAt)}>{age(modifiedAt)}</Label>
+              <Label color={labelColor(modifiedAt.unix())}>{age(modifiedAt.unix())}</Label>
             </GridItem>
           </Grid>
         </CardTitle>
         <CardBody>
-
-          {/* // TODO Find a better place to link the repo. maybe just the header?
-            <a href={'https://github.com/' + repository?.name?.toLowerCase()}>
-            <Label variant="outline" color="grey" icon={<RepositoryIcon />}>
-              {repository?.name?.replace('RedHatInsights/', '')}
-            </Label>
-          </a> */}
 
           {/* TODO put pullrequest labels here. */}
 
@@ -47,7 +39,7 @@ const PullRequestCard = (pullRequest) => {
           <Text component="small" style={{ color: 'lightgrey' }}>
             {/* TODO This could live somewhere else as well */}
             <UpdateBy reporter={author?.username} />{' '}
-            {modifiedAt ? formatDistance(new Date(modifiedAt * 1000), new Date(), { addSuffix: true }) : ''}
+            {modifiedAt.fromNow()}
           </Text>
         </CardFooter>
       </Card>
